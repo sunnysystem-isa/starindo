cf049a15ea7a1c16fbfebf0f7671e6c8a6825c41
Fureya <fathur.rohman2535@gmail.com>
2023-07-25 11:00:14 +0700

    Update: - Create Datatable button

--- resources/views/production.blade.php
+++ resources/views/production.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.main')
 
 @section('title', 'Contacts')
-
+<style>
+    .buttons-excel{
+        padding: 10px;
+        padding-left: 15px;
+        padding-right: 15px;
+        border-radius: 5px;
+        margin-right: 10px;
+        margin-bottom: 0;
+        border: 0;
+        background-color: #d1dbd5 !important;
+    }
+    .buttons-pdf{
+        padding: 10px;
+        padding-left: 15px;
+        padding-right: 15px;
+        border-radius: 5px;
+        margin-right: 10px;
+        border: 0;
+        background-color: #d1dbd5 !important;
+    }
+</style>
 @section('container')
 
 <!--End :: Customer Menu-->
@@ -401,9 +421,44 @@
 @endsection
 
 @section('js-script')  
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
+{{-- <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.js"></script> --}}
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/dataTables.jqueryui.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.jqueryui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+
+
+<script>
+    $(document).ready(function() {
+    var table = $('#example').DataTable( {
+        lengthChange: false,
+        buttons: [
+            {
+                extend: 'excelHtml5',
+                title: 'Production'
+            },
+            {
+                extend: 'pdfHtml5',
+                title: 'Production'
+            }
+        ]
+    } );
+ 
+    table.buttons().container()
+        .insertBefore( '#example_filter' ).appendTo( '#example_wrapper .col-md-6:eq(0)' );
+;
+} );
+</script>
 
 <script>
     $(document).ready(function(){
@@ -411,6 +466,7 @@
             $('#kt_modal_create_customer').modal('show');
     });
 </script>
+
 
 
 
